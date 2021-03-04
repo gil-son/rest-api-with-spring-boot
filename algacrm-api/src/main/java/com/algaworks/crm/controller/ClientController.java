39f32bf0c60b4e9baf83fb43fb725a5993bb96d5
gil-son <gilszon@gmail.com>
2021-03-04 02:19:44 -0300

    Client Controller

--- algacrm-api/src/main/java/com/algaworks/crm/controller/ClientController.java
+++ algacrm-api/src/main/java/com/algaworks/crm/controller/ClientController.java
@@ -2,19 +2,39 @@ package com.algaworks.crm.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.crm.model.Client;
+import com.algaworks.crm.repository.ClientRepository;
 
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
 	
+	@Autowired
+	private ClientRepository clientRepository;
+	
 	@GetMapping
 	public List<Client> list() {
-	
+		return clientRepository.findAll(); // Method from Jpa
 	}
 
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Client add(@RequestBody Client client) {
+		
+		return clientRepository.save(client);
+		
+	}
+	
+	
+	
+	
 }
